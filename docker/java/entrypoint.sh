f58fd47cd4250e02d59a5825dde954a48f62e1a8
ijaatit <ilyassjaatit@gmail.com>
2022-08-22 17:30:35 +0200

    add vsftpd and services QUEUE

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
 set -o errexit
 set -o pipefail
-
+if [ -z ${AMM_QUEUE} ];then
+  echo "AMM_QUEUE is unset";
+  exit;
+  else
+    echo "AMM_QUEUE is set to '${AMM_QUEUE}'";
+fi
+sleep 3
 mvn clean install
 cd /tibco/target/classes
 CLASSPATH=/tibco/target/libs/jms-2.0.jar:${CLASSPATH} \
@@ -10,5 +15,5 @@ CLASSPATH=/tibco/target/libs/tibjms-8.2.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/log4j-api-2.17.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/log4j-core-2.17.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/dotenv-java-2.2.4.jar:${CLASSPATH} \
-java net.gisce.tibjmsMsgConsumer -user "${TIBCO_USER}" -password "${TIBCO_PASSWORD}" -queue  "AMM.LPOData" -server "tcp://localhost:7224"
+java net.gisce.tibjmsMsgConsumer -user "${TIBCO_USER}" -password "${TIBCO_PASSWORD}" -queue  "${AMM_QUEUE}" -server "tcp://localhost:7224"
 "$@"
