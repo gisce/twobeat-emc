93b8efc7e0cb3de69fe96e0c70f6a8476f69bfea
ijaatit <ilyassjaatit@gmail.com>
2022-08-22 14:52:23 +0200

    Add add CLASSPATH  in one line

--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -4,11 +4,11 @@ set -o errexit
 set -o pipefail
 
 mvn clean install
-export CLASSPATH=/tibco/target/libs/jms-2.0.jar:${CLASSPATH}
-export CLASSPATH=/tibco/target/libs/tibjms-8.2.2.jar:${CLASSPATH}
-export CLASSPATH=/tibco/target/libs/log4j-api-2.17.2.jar:${CLASSPATH}
-export CLASSPATH=/tibco/target/libs/log4j-core-2.17.2.jar:${CLASSPATH}
-export CLASSPATH=/tibco/target/libs/dotenv-java-2.2.4.jar:${CLASSPATH}
 cd /tibco/target/classes
+CLASSPATH=/tibco/target/libs/jms-2.0.jar:${CLASSPATH} \
+CLASSPATH=/tibco/target/libs/tibjms-8.2.2.jar:${CLASSPATH} \
+CLASSPATH=/tibco/target/libs/log4j-api-2.17.2.jar:${CLASSPATH} \
+CLASSPATH=/tibco/target/libs/log4j-core-2.17.2.jar:${CLASSPATH} \
+CLASSPATH=/tibco/target/libs/dotenv-java-2.2.4.jar:${CLASSPATH} \
 java net.gisce.tibjmsMsgConsumer -user "${TIBCO_USER}" -password "${TIBCO_PASSWORD}" -queue  "AMM.LPOData" -server "tcp://localhost:7224"
 "$@"
