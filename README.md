fbac3e36c802f9f3679f9b67811713d0059651ab
ijaatit <ilyassjaatit@gmail.com>
2022-09-01 14:05:00 +0200

    mvn install and clean in building container only

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # twobeat-emc
-Tibco ems connection
+Tibco ems connection and download xml file 
+## system requirements
+    - docker-compose > 1.29.2
+    - docker  > 20.10.17
+## Setup local development environment (with docker-composer)
 
-## Setup local development environment (with docker)
-cp .env.sample ./tibco/src/resources/.env
-
+1. cp .env.sample ./tibco/src/resources/.env
+   2. Edit and config env
+3. Build image  
+    4. `docker-compose  build`
+5. run containers
+   6. `docker-compose  up -d`
--- docker/java/entrypoint.sh
+++ docker/java/entrypoint.sh
@@ -7,13 +7,18 @@ if [ -z ${AMM_QUEUE} ];then
   else
     echo "AMM_QUEUE is set to '${AMM_QUEUE}'";
 fi
-sleep 3
-mvn clean install
+
+if [ "${APP_ENVIORMENT}" = "prod" ]; then
+  mvn compiler
+else
+  mvn compiler
+fi
+
 cd /tibco/target/classes
 CLASSPATH=/tibco/target/libs/jms-2.0.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/tibjms-8.2.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/log4j-api-2.17.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/log4j-core-2.17.2.jar:${CLASSPATH} \
 CLASSPATH=/tibco/target/libs/dotenv-java-2.2.4.jar:${CLASSPATH} \
-java net.gisce.tibjmsMsgConsumer -user "${TIBCO_USER}" -password "${TIBCO_PASSWORD}" -queue  "${AMM_QUEUE}" -server "tcp://localhost:7224"
+java net.gisce.tibjmsMsgConsumer -user "${TIBCO_USER}" -password "${TIBCO_PASSWORD}" -queue  "${AMM_QUEUE}" -server "${TIBCO_SERVER_URL}"
 "$@"
